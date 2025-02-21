50df41a233034a77eccf1cc238bac282eaf23b5e
Palash Joshi <palash.joshi@advantal.net>
2025-02-21 16:35:25 +0530

    firrst commit

--- src/main/java/com/employee/demo/controller/EmployeeController.java
+++ src/main/java/com/employee/demo/controller/EmployeeController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.employee.demo.request.EmployeeRequest;
@@ -49,4 +51,10 @@ public class EmployeeController {
     public Map<Long, EmployeeResponse> getEmployeesByIdMap() {
         return service.getEmployeesByIdMap();
     }
+  //14
+    @DeleteMapping("/{id}")  
+    public String deleteEmployee(@RequestParam Long id) {
+        employeeService.deleteEmployee(id);
+        return "Employee deleted successfully!";
+   }
 }
