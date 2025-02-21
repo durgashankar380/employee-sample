7873ffbf34954e7b0e0c1f078eb1fe37b2006e48
Palash Joshi <palash.joshi@advantal.net>
2025-02-21 16:50:12 +0530

    update controller

--- src/main/java/com/employee/demo/controller/EmployeeController.java
+++ src/main/java/com/employee/demo/controller/EmployeeController.java
@@ -27,34 +27,97 @@ public class EmployeeController {
 
    
 
-    @PostMapping("/add")
-    public EmployeeResponse addEmployee(@RequestBody EmployeeRequest employeeRequest) {
-        return service.addEmployee(employeeRequest);
+    //1
+    @PostMapping("/add") 
+    public Employee createEmployee(@RequestBody RequestEmployee requestEmployee) {
+        return employeeService.saveEmployee(requestEmployee);
     }
     
-    @GetMapping("/total-salary")
+    //2
+    @GetMapping("/total-Salary")
     public Map<String, Double> getTotalSalaryPerDepartment() {
-        return service.getTotalSalaryPerDepartment();
+    	return employeeService.getTotalSalaryPerDepartment();
     }
+    
 
+    //3
     @GetMapping("/grouped-by-department")
-    public Map<String, List<EmployeeResponse>> getEmployeesGroupedByDepartment() {
-        return service.getEmployeesGroupedByDepartment();
+    public Map<String, List<Employee>> getEmployeesGroupedByDepartment() {
+    	return employeeService.getEmployeesGroupedByDepartment();
     }
-
+    
+    
+    //4
     @GetMapping("/unique-departments")
-    public Set<String> getUniqueEmployeeDepartments() {
-        return service.getUniqueEmployeeDepartments();
+    public ResponseEntity<Set<String>> getUniqueDepartments() {
+    	return ResponseEntity.ok(employeeService.getUniqueDepartments());
+    }
+    
+    //5
+    @GetMapping("/map")
+    public Map<Long, Employee> getEmployeesAsMap() {
+    	return employeeService.getEmployeesAsMap();
+    }
+    
+    //6
+    @PostMapping("/add-multiple")
+    public List<Employee> createEmployees(@RequestBody List<RequestEmployee> requestEmployees) {
+    	return employeeService.saveEmployees(requestEmployees);
+    }
+    
+    
+    //7
+    @GetMapping("/sorted-by-salary")
+    public List<Employee> getEmployeesSortedBySalaryDesc() {
+        return employeeService.getEmployeesSortedBySalaryDesc();
+    }
+    
+    //8
+    @GetMapping("/names")
+    public List<String> getEmployeeNames() {
+        return employeeService.getEmployeeNames();
+    }
+    
+    //9
+    @GetMapping("/count-by-department")
+    public Map<String, Integer> getStudentCountByDepartment() {
+        return employeeService.getStudentCountByDepartment();
+    }
+    
+    //10
+    @GetMapping("/fifo")
+    public List<Employee> getEmployeesInFIFOOrder() {
+        return employeeService.getEmployeesInFIFOOrder();
+    }
+    
+    //11
+    @GetMapping("/lifo-employees")
+    public List<Employee> getEmployeesInLIFOOrder() {
+        return employeeService.getEmployeesInLIFOOrder();
     }
 
-    @GetMapping("/employee-map")
-    public Map<Long, EmployeeResponse> getEmployeesByIdMap() {
-        return service.getEmployeesByIdMap();
+    
+    //12
+     @GetMapping("/{id}")  
+    public ResponseEmployee getEmployeeById(@RequestParam Long id) {
+        return employeeService.getEmployeeById(id);
     }
-  //14
+    
+    
+    
+  //13
+   @PutMapping("/{id}")  
+    public Employee updateEmployee(@RequestParam Long id, @RequestBody RequestEmployee requestEmployee) {
+        return employeeService.updateEmployee(id, requestEmployee);
+    }
+
+    
+    //14
     @DeleteMapping("/{id}")  
     public String deleteEmployee(@RequestParam Long id) {
         employeeService.deleteEmployee(id);
         return "Employee deleted successfully!";
    }
+   
+    
 }
