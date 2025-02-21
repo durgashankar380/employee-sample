2359bd660e844831a7377101848e23b96ff693c9
Palash Joshi <palash.joshi@advantal.net>
2025-02-21 16:59:05 +0530

    update Service

--- src/main/java/com/employee/demo/service/EmployeeService.java
+++ src/main/java/com/employee/demo/service/EmployeeService.java
@@ -9,14 +9,47 @@ import com.employee.demo.response.EmployeeResponse;
 
 public interface EmployeeService {
 
+	//1
+    Employee saveEmployee(RequestEmployee requestEmployee);
+ 
+    //2
+    Map<String, Double> getTotalSalaryPerDepartment();
+    
+    //3	
+    Map<String, List<Employee>> getEmployeesGroupedByDepartment();
+    
+    //4
+    Set<String> getUniqueDepartments();
+    
+    //5
+    Map<Long, Employee> getEmployeesAsMap();
+    
+    //6
+    List<Employee> saveEmployees(List<RequestEmployee> requestEmployees);
+    
+    
+    //7
+    List<Employee> getEmployeesSortedBySalaryDesc();
+    
+    //8
+    List<String> getEmployeeNames();
+    
+    //9
+    Map<String, Integer> getStudentCountByDepartment();
+    
+    //10
+    List<Employee> getEmployeesInFIFOOrder();
+    
+    //11
+    List<Employee> getEmployeesInLIFOOrder();
 
-	EmployeeResponse addEmployee(EmployeeRequest employeeRequest);
+    //12
+    ResponseEmployee getEmployeeById(Long id);
+    
+    //13
+    Employee updateEmployee(Long id, RequestEmployee requestEmployee);
+    
+    //14
+    void deleteEmployee(Long id);  
 
-	Map<String, Double> getTotalSalaryPerDepartment();
-
-	Map<String, List<EmployeeResponse>> getEmployeesGroupedByDepartment();
-
-	Set<String> getUniqueEmployeeDepartments();
-
-	Map<Long, EmployeeResponse> getEmployeesByIdMap();
 }
