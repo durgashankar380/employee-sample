2c2554e9b47cc44125659c29741e216678bd0b33
Yash Yadav <yash.yadav@advantal.net>
2025-02-21 16:00:03 +0530

    initial commit

--- src/main/java/com/employee/demo/controller/EmployeeController.java
+++ src/main/java/com/employee/demo/controller/EmployeeController.java
@@ -23,11 +23,7 @@ public class EmployeeController {
         this.service = service;
     }
 
-   /**
-    * 
-    * @param employeeRequest
-    * @return
-    */
+   
 
     @PostMapping("/add")
     public EmployeeResponse addEmployee(@RequestBody EmployeeRequest employeeRequest) {
